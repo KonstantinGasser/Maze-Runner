8a1dbc60be91fe746c22347de6849bdeee569833
Konstantin Gasser <1810507@stud.hs-mannheim.de>
2020-01-14 00:03:36 +0100

    new gifs

--- src/constants.py
+++ src/constants.py
@@ -6,14 +6,14 @@ GREEN: Tuple[int, int, int] = (0, 255, 0)
 RED  : Tuple[int, int, int] = (255, 0, 0)
 BLUE : Tuple[int, int, int] = (0, 0, 255)
  
-WINDOW_WIDTH : int = 900
-WINDOW_HEIGHT: int = 800
+WINDOW_WIDTH : int = 500
+WINDOW_HEIGHT: int = 500
 
-WIDTH : int = 75
-HEIGHT: int= 75
+WIDTH : int = 45
+HEIGHT: int= 45
  
 
-COLS: int = (WINDOW_WIDTH-100) // WIDTH
+COLS: int = (WINDOW_WIDTH) // WIDTH
 ROWS: int = WINDOW_HEIGHT // HEIGHT
 
 WINDOW_SIZE: Tuple[int, int] = (WINDOW_WIDTH, WINDOW_HEIGHT + 5)
